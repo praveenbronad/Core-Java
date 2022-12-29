30c047ac86760f43dce1aeb53506c3fdec194fc1
Praveen B Ronad <Praveen B Ronad@LAPTOP-LCLJAJ2G>
2022-12-29 13:19:16 +0530

    new testcase

---
+++ src/arrayPracticePrograms/NewTestCase.java
@@ -0,0 +1,10 @@
+package arrayPracticePrograms;
+
+public class NewTestCase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hello");
+	}
+
+}
